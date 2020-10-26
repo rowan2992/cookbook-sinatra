0fb734411c34a66bfce8bfaee989364231043017
rowan douglas <rowan.douglas29@gmail.com>
2020-10-26 19:37:44 -0400

    just used the solutions. i haven't really completed this to be honest

--- app.rb
+++ app.rb
@@ -7,6 +7,28 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
+require_relative "cookbook"
+require_relative "recipe"
+
 get '/' do
-  'Hello world!'
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  @recipes = cookbook.all
+  erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/recipes' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  recipe = Recipe.new(params[:name], params[:description])
+  cookbook.add(recipe)
+  redirect to '/'
+end
+
+get '/recipes/:index' do
+  cookbook = Cookbook.new(File.join(__dir__, 'recipes.csv'))
+  cookbook.remove_at(params[:index].to_i)
+  redirect to '/'
 end
